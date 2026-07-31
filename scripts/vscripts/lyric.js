7c77f719d6113eba2be23b8a2e33b71be29f3b1b
兎RB <dnwls5918@naver.com>
2026-07-31 15:26:29 +0900

    Do not use. Testing

---
+++ scripts/vscripts/lyric.js
@@ -0,0 +1,385 @@
+// map ze_deadcore_r
+// workshop id 3331345627
+// -----------------------
+// Do not use. Testing
+// -----------------------
+
+//keep the text centered regardless of screen resolution
+import { Instance } from 'cs_script/point_script';
+
+Instance.OnScriptInput("Jpanese", () => {
+
+    Instance.EntFireAtTarget({
+        target: "lyric_relay",
+        input: "CountPlayersInZone",
+        value: "",
+        delay: 0.0
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u5FA1\u4F3D\u8A71\u0028\u30D5\u30A7\u30A2\u30EA\u30FC\u30C6\u30A4\u30EB\u0029\u306F\nFeariiteiru wa",
+        delay: 0.0
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u3055\u3063\u304D\u6B7B\u3093\u3060\u307F\u305F\u3044\nSakki shinda mitai",
+        delay: 5.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u7149\u74E6\u306E\u75C5\u68DF\u3067\nRenga no byoutou de",
+        delay: 10.6
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u3046\u307E\u304F\u6B4C\u3048\u306A\u304F\u3066\nUmaku utaenakute",
+        delay: 15.9
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u9727\u306B\u7159\u308B\u591C\nKiri ni kemuru yoru",
+        delay: 21.3
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u6D6E\u304B\u3079\u8D64\u3044\u6708\nUkabe akai tsuki",
+        delay: 26.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u307B\u3089\u898B\u3066\u79C1\u3092\nHora mite watashi wo",
+        delay: 31.8
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u76EE\u3092\u9038\u3089\u3055\u306A\u3044\u3067\nMe wo sorasanaide",
+        delay: 37.3
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u9ED2\u3044\u9244\u683C\u5B50\u306E\u4E2D\u3067\nKuroi tetsu goushi no naka de",
+        delay: 43.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u79C1\u306F\u751F\u307E\u308C\u3066\u304D\u305F\u3093\u3060\nWatashi wa umarete kitanda",
+        delay: 45.8
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u60AA\u610F\u306E\u4EE3\u511F\u3092\u9858\u3048\nAkui no daishou wo negae",
+        delay: 48.4
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u671B\u3080\u304C\u307E\u307E\u306B\u304A\u524D\u306B\nNozomu ga mama ni omae ni",
+        delay: 50.7
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u3055\u3041\u4E0E\u3048\u3088\u3046\u6B63\u7FA9\u3092\nSaa ataeyou seigi wo",
+        delay: 53.9
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u58CA\u3057\u3066\u58CA\u3055\u308C\u308B\u524D\u306B\nKowashite kowasareru mae ni",
+        delay: 56.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u56E0\u679C\u306E\u4EE3\u511F\u3092\u6255\u3044\nInga no daishou wo harai",
+        delay: 59.1
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u5171\u306B\u884C\u3053\u3046\u0020\u540D\u3082\u306A\u304D\nTomo ni yukou na mo naki",
+        delay: 60.9
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u602A\u7269\nKaibutsu",
+        delay: 63.7
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u9ED2\u3044\u96E8\u0020\u964D\u3089\u305B\u3053\u306E\u7A7A\nKuroi ame furase kono sora",
+        delay: 64.7
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u79C1\u306F\u671B\u307E\u308C\u306A\u3044\u3082\u306E\nWatashi wa nozomarenai mono",
+        delay: 67.4
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u3072\u3073\u5272\u308C\u305F\u30CE\u30A4\u30ED\u30FC\u30BC\nHibi wareta noirooze",
+        delay: 70.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u611B\u3059\u540C\u7F6A\u306E\u508D\u89B3\u8005\u9054\u306B\nAisu douzai no boukansha tachi ni",
+        delay: 72.4
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u3055\u3041\u4ECA\u3075\u308B\u3048\u6B63\u7FA9\u3092\nSaa ima furue seigi wo",
+        delay: 75.7
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u6D88\u305B\u306A\u3044\u50B7\u3092\u62B1\u304D\u3057\u3081\u3066\nKesenai kizu wo dakishimete",
+        delay: 78.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u3053\u306E\u8EAB\u4F53\u3092\u53D7\u3051\u5165\u308C\nKono karada wo uke ire",
+        delay: 80.6
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "\u5171\u306B\u884C\u3053\u3046\u0020\u540D\u524D\u306E\u306A\u3044\nTomo ni yukou namae no nai",
+        delay: 83.1
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "Kill",
+        value: "",
+        delay: 86.1
+    });
+});
+
+Instance.OnScriptInput("English", () => {
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "A dying fairy tale",
+        delay: 0.0
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Just took its last breath",
+        delay: 5.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "It once sung through these halls",
+        delay: 10.6
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Though blocked by brick hospital walls",
+        delay: 15.9
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Fog is covering the night",
+        delay: 21.3
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "The blood red moon floating up high",
+        delay: 26.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Hey, can you keep your eyes on me",
+        delay: 31.8
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "And make it everything you see",
+        delay: 37.3
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Behind these bars behind this world of iron",
+        delay: 43.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "My soul was born and now at last it's my turn",
+        delay: 45.8
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "So in exchange for my complete revenge",
+        delay: 48.4
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "I'll grantyou anywish spoken from your lips",
+        delay: 51.0
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "We'll bring forth the mercy and the justice",
+        delay: 53.9
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Destroy them all before they even touch us",
+        delay: 56.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "We'll pay the price a karma paradise",
+        delay: 59.1
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "And walk ahead as one becoming",
+        delay: 61.7
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "We've done",
+        delay: 63.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Net the rain continue falling on this",
+        delay: 64.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Unwanted soul in case you haven't noticed",
+        delay: 67.2
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "The guilty sin of my neurotic mind",
+        delay: 70.1
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "I share it with the strangers passing me by",
+        delay: 72.7
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "So bring forth the mercy and the justice",
+        delay: 75.5
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "Embrace these scars i know will never vanish",
+        delay: 78.0
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "My whose body accepting all of me",
+        delay: 80.6
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "SetMessage",
+        value: "We'll walk ahead as one becoming a monster",
+        delay: 83.1
+    });
+
+    Instance.EntFireAtTarget({
+        target: "lyric_hud",
+        input: "Kill",
+        value: "",
+        delay: 86.1
+    });
+});
