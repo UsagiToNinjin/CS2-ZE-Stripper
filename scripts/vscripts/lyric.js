4a8d11cce868c46ac2712c13593fbc618a7ffa31
兎RB <dnwls5918@naver.com>
2026-07-31 15:34:21 +0900

    Do not use. Testing

--- scripts/vscripts/lyric.js
+++ scripts/vscripts/lyric.js
@@ -19,175 +19,175 @@ Instance.OnScriptInput("Jpanese", () => {
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u5FA1\u4F3D\u8A71\u0028\u30D5\u30A7\u30A2\u30EA\u30FC\u30C6\u30A4\u30EB\u0029\u306F\nFeariiteiru wa",
+        value: "Feariiteiru wa\n\u5FA1\u4F3D\u8A71\u0028\u30D5\u30A7\u30A2\u30EA\u30FC\u30C6\u30A4\u30EB\u0029\u306F",
         delay: 0.0
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u3055\u3063\u304D\u6B7B\u3093\u3060\u307F\u305F\u3044\nSakki shinda mitai",
+        value: "Sakki shinda mitai\n\u3055\u3063\u304D\u6B7B\u3093\u3060\u307F\u305F\u3044",
         delay: 5.2
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u7149\u74E6\u306E\u75C5\u68DF\u3067\nRenga no byoutou de",
+        value: "Renga no byoutou de\n\u7149\u74E6\u306E\u75C5\u68DF\u3067",
         delay: 10.6
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u3046\u307E\u304F\u6B4C\u3048\u306A\u304F\u3066\nUmaku utaenakute",
+        value: "Umaku utaenakute\n\u3046\u307E\u304F\u6B4C\u3048\u306A\u304F\u3066",
         delay: 15.9
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u9727\u306B\u7159\u308B\u591C\nKiri ni kemuru yoru",
+        value: "Kiri ni kemuru yoru\n\u9727\u306B\u7159\u308B\u591C",
         delay: 21.3
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u6D6E\u304B\u3079\u8D64\u3044\u6708\nUkabe akai tsuki",
+        value: "Ukabe akai tsuki\n\u6D6E\u304B\u3079\u8D64\u3044\u6708",
         delay: 26.5
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u307B\u3089\u898B\u3066\u79C1\u3092\nHora mite watashi wo",
+        value: "Hora mite watashi wo\n\u307B\u3089\u898B\u3066\u79C1\u3092",
         delay: 31.8
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u76EE\u3092\u9038\u3089\u3055\u306A\u3044\u3067\nMe wo sorasanaide",
+        value: "Me wo sorasanaide\n\u76EE\u3092\u9038\u3089\u3055\u306A\u3044\u3067",
         delay: 37.3
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u9ED2\u3044\u9244\u683C\u5B50\u306E\u4E2D\u3067\nKuroi tetsu goushi no naka de",
+        value: "Kuroi tetsu goushi no naka de\n\u9ED2\u3044\u9244\u683C\u5B50\u306E\u4E2D\u3067",
         delay: 43.2
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u79C1\u306F\u751F\u307E\u308C\u3066\u304D\u305F\u3093\u3060\nWatashi wa umarete kitanda",
+        value: "Watashi wa umarete kitanda\n\u79C1\u306F\u751F\u307E\u308C\u3066\u304D\u305F\u3093\u3060",
         delay: 45.8
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u60AA\u610F\u306E\u4EE3\u511F\u3092\u9858\u3048\nAkui no daishou wo negae",
+        value: "Akui no daishou wo negae\n\u60AA\u610F\u306E\u4EE3\u511F\u3092\u9858\u3048",
         delay: 48.4
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u671B\u3080\u304C\u307E\u307E\u306B\u304A\u524D\u306B\nNozomu ga mama ni omae ni",
+        value: "Nozomu ga mama ni omae ni\n\u671B\u3080\u304C\u307E\u307E\u306B\u304A\u524D\u306B",
         delay: 50.7
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u3055\u3041\u4E0E\u3048\u3088\u3046\u6B63\u7FA9\u3092\nSaa ataeyou seigi wo",
+        value: "Saa ataeyou seigi wo\n\u3055\u3041\u4E0E\u3048\u3088\u3046\u6B63\u7FA9\u3092",
         delay: 53.9
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u58CA\u3057\u3066\u58CA\u3055\u308C\u308B\u524D\u306B\nKowashite kowasareru mae ni",
+        value: "Kowashite kowasareru mae ni\n\u58CA\u3057\u3066\u58CA\u3055\u308C\u308B\u524D\u306B",
         delay: 56.5
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u56E0\u679C\u306E\u4EE3\u511F\u3092\u6255\u3044\nInga no daishou wo harai",
+        value: "Inga no daishou wo harai\n\u56E0\u679C\u306E\u4EE3\u511F\u3092\u6255\u3044",
         delay: 59.1
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u5171\u306B\u884C\u3053\u3046\u0020\u540D\u3082\u306A\u304D\nTomo ni yukou na mo naki",
+        value: "Tomo ni yukou na mo naki\n\u5171\u306B\u884C\u3053\u3046\u0020\u540D\u3082\u306A\u304D",
         delay: 60.9
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u602A\u7269\nKaibutsu",
+        value: "Kaibutsu\n\u602A\u7269",
         delay: 63.7
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u9ED2\u3044\u96E8\u0020\u964D\u3089\u305B\u3053\u306E\u7A7A\nKuroi ame furase kono sora",
+        value: "Kuroi ame furase kono sora\n\u9ED2\u3044\u96E8\u0020\u964D\u3089\u305B\u3053\u306E\u7A7A",
         delay: 64.7
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u79C1\u306F\u671B\u307E\u308C\u306A\u3044\u3082\u306E\nWatashi wa nozomarenai mono",
+        value: "Watashi wa nozomarenai mono\n\u79C1\u306F\u671B\u307E\u308C\u306A\u3044\u3082\u306E",
         delay: 67.4
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u3072\u3073\u5272\u308C\u305F\u30CE\u30A4\u30ED\u30FC\u30BC\nHibi wareta noirooze",
+        value: "Hibi wareta noirooze\n\u3072\u3073\u5272\u308C\u305F\u30CE\u30A4\u30ED\u30FC\u30BC",
         delay: 70.2
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u611B\u3059\u540C\u7F6A\u306E\u508D\u89B3\u8005\u9054\u306B\nAisu douzai no boukansha tachi ni",
+        value: "Aisu douzai no boukansha tachi ni\n\u611B\u3059\u540C\u7F6A\u306E\u508D\u89B3\u8005\u9054\u306B",
         delay: 72.4
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u3055\u3041\u4ECA\u3075\u308B\u3048\u6B63\u7FA9\u3092\nSaa ima furue seigi wo",
+        value: "Saa ima furue seigi wo\n\u3055\u3041\u4ECA\u3075\u308B\u3048\u6B63\u7FA9\u3092",
         delay: 75.7
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u6D88\u305B\u306A\u3044\u50B7\u3092\u62B1\u304D\u3057\u3081\u3066\nKesenai kizu wo dakishimete",
+        value: "Kesenai kizu wo dakishimete\n\u6D88\u305B\u306A\u3044\u50B7\u3092\u62B1\u304D\u3057\u3081\u3066",
         delay: 78.2
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u3053\u306E\u8EAB\u4F53\u3092\u53D7\u3051\u5165\u308C\nKono karada wo uke ire",
+        value: "Kono karada wo uke ire\n\u3053\u306E\u8EAB\u4F53\u3092\u53D7\u3051\u5165\u308C",
         delay: 80.6
     });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
         input: "SetMessage",
-        value: "\u5171\u306B\u884C\u3053\u3046\u0020\u540D\u524D\u306E\u306A\u3044\nTomo ni yukou namae no nai",
+        value: "Tomo ni yukou namae no nai\n\u5171\u306B\u884C\u3053\u3046\u0020\u540D\u524D\u306E\u306A\u3044",
         delay: 83.1
     });
 
@@ -200,6 +200,13 @@ Instance.OnScriptInput("Jpanese", () => {
 });
 
 Instance.OnScriptInput("English", () => {
+
+    Instance.EntFireAtTarget({
+        target: "lyric_relay",
+        input: "CountPlayersInZone",
+        value: "",
+        delay: 0.0
+    });
 
     Instance.EntFireAtTarget({
         target: "lyric_hud",
